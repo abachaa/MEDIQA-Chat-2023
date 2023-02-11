f44d563de25dd6b1c7fc4de78799672717a431ce
Asma Ben Abacha <34075625+abachaa@users.noreply.github.com>
2023-02-10 19:32:04 -0500

    Add files via upload

---
+++ submission_checker.py
@@ -0,0 +1,33 @@
+import csv
+import os
+
+filepath = ""
+
+# Check that the file path exists
+if not os.path.exists(filepath):
+    raise ValueError("File path does not exist")
+
+# Extract the file name from the file path
+filename = os.path.basename(filepath)
+
+# Check that the file name starts with "taskA", "taskB", or "taskC"
+if not (filename.startswith("taskA") or filename.startswith("taskB") or filename.startswith("taskC")):
+    raise ValueError("File name must start with 'taskA', 'taskB', or 'taskC'")
+
+# Check that the file is a CSV file
+if not filename.endswith(".csv"):
+    raise ValueError("File must be a CSV file")
+
+# Open the file and check that it has 2 columns
+with open(filepath, "r") as file:
+    reader = csv.reader(file)
+    header = next(reader)
+    if len(header) != 2:
+        raise ValueError("CSV file must have 2 columns")
+    
+    # Check that the first column contains test IDs: numbers preceded by "taskA", "taskB", or "taskC"
+    for row in reader:
+        if not (row[0].startswith("taskA") or row[0].startswith("taskB") or row[0].startswith("taskC") or row[0].startswith("D2N") ) or not row[0][5:].isdigit():
+            raise ValueError("First column must contain test IDs.")
+
+print("File is valid.")
